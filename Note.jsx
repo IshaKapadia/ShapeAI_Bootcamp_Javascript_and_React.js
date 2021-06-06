2d579bf8d4d0177ca904959c0453d753c15eeefd
IshaKapadia <76873071+IshaKapadia@users.noreply.github.com>
2021-06-07 02:59:58 +0530

    Add files via upload

---
+++ Note.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+function Note() {
+  return (
+    <div className="note">
+      <h1>Javascript and Reactjs</h1>
+      <p>
+        The instructor during the session was Mr. Shaurya Sinha (Data Analyst
+        Intern at Jio). I got to learn a lot during these 7 days and it was an
+        amazing experience learning with SHAPEAI. ,and everything was explained
+        from the very basics so that anyone with zero experience on programming
+        can learn. I enjoyed these 7 days
+      </p>
+    </div>
+  );
+}
+
+export default Note;
---
+++ index.js
@@ -0,0 +1,12 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./components/App";
+//import Heading from "./Heading";
+//import List from "./List";
+
+ReactDOM.render(
+  <div>
+    <App />
+  </div>,
+  document.getElementById("root")
+);
